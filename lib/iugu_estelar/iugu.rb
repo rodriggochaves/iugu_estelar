433f722d43e0618d498c38bd1995525f15d3cd96
Rodrigo A Chaves <rodriggo.chaves@gmail.com>
2020-03-11 18:39:36 -0300

    feature (subscriptions): download all subscriptions

--- lib/iugu_estelar/iugu.rb
+++ lib/iugu_estelar/iugu.rb
@@ -1,15 +1,16 @@
 require 'json'
 require 'rest-client'
+require 'dotenv/load'
 
 module IuguEstelar
   module Iugu
-    def self.request_to_iugu(params)
+    def self.request_to_iugu(method, url, params)
       payload = params.merge({
         api_token: ENV['IUGU']
       })
       response = RestClient::Request.execute(
-        url: 'https://api.iugu.com/v1/invoices',
-        method: :get,
+        url: "https://api.iugu.com/v1/#{url}",
+        method: method,
         headers: {
           "Content-Type": "application/json"
         },
@@ -19,16 +20,30 @@ module IuguEstelar
     end
 
     def self.count_invoices
-      request_to_iugu({})["totalItems"]
+      request_to_iugu(:get, 'invoices', {})["totalItems"]
     end
 
     def self.list_invoices_page page
-      request_to_iugu({ start: page })["items"]
+      request_to_iugu(:get, 'invoices',  { start: page })["items"]
     end
 
     def self.list_all_invoices
       (0..count_invoices).to_a.select { |n| n % 100 == 0 }.flat_map do |page|
         list_invoices_page(page)
+      end
+    end
+
+    def self.count_all_subscriptions
+      request_to_iugu(:get, 'subscriptions', {})["totalItems"]
+    end
+
+    def self.list_subscriptions_page(page)
+      request_to_iugu(:get, 'subscriptions',  { start: page })["items"]
+    end
+
+    def self.list_all_subscriptions
+      (0..count_all_subscriptions).to_a.select { |n| n % 100 == 0 }.flat_map do |page|
+        list_subscriptions_page(page)
       end
     end
   end
